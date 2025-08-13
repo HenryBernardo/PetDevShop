5e09c9ce905227994db60af4eb513611e37abec4
Henry Bernardo Lopes Kruger <henrybernardolk@gmail.com>
2025-08-13 13:59:03 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes)
 
 server.use((req, res)=>{
-  res.send('página nao encontrada')
+  res.render('pages/404')
 })
 
 server.listen(process.env.PORT);
